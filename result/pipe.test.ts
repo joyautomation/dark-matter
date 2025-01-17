0a0a3935d577edcf6bccdb22e6de33df4283b393
James Joy <joyja@joyautomation.com>
2025-01-17 09:25:29 -0800

    changed testing lib imports to the correct reference

--- result/pipe.test.ts
+++ result/pipe.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
+import { assertEquals } from "@std/assert";
 import { createSuccess, createFail } from "./result.ts";
 import { resultPipe } from "./pipe.ts";
 
